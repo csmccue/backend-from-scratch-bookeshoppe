dbf717b7c76b21df7986e3288efa94cac48a8981
Connor McCue <connor.mccue@comcast.net>
2022-11-08 16:33:50 -0800

    books test 1

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,15 +1,25 @@
 const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
-// const request = require('supertest');
-// const app = require('../lib/app');
+const request = require('supertest');
+const app = require('../lib/app');
 
 describe('backend-express-template routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
-  it('example test - delete me!', () => {
-    expect(1).toEqual(1);
+
+
+
+  // test 1 show all books
+  it('return all books', async () => {
+    const res = await request(app).get('/books');
+    expect(res.body.length).toEqual(6);
   });
+
+
+
+
+
   afterAll(() => {
     pool.end();
   });
